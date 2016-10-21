a80ea922efcf0d963e3858ead565bbfa94774abf
yycho0108 <jchocholate@gmail.com>
2016-10-21 16:25:19 -0400

    bksub malfunctioning

--- Stereo/handleStereoData.py
+++ Stereo/handleStereoData.py
@@ -11,6 +11,7 @@ from sensor_msgs.msg import PointCloud2, Image
 from stereo_msgs.msg import DisparityImage
 
 br = CvBridge()
+fgbg = cv2.BackgroundSubtractorMOG2()
 
 def circleArea(r):
     return r*r*3.14159265358979
@@ -50,6 +51,18 @@ def handleCloud(cloud):
     #print "Height : {}, Width : {}".format(cloud.height, cloud.width)
     pass
 
+def handleImage(msg):
+    global br
+    im = br.imgmsg_to_cv2(msg)
+    cv2.imshow('image', im)
+
+    #im = cv2.fastNlMeansDenoisingColored(im)
+    im = cv2.GaussianBlur(im,(3,3),0) 
+    #im = cv2.pyrMeanShiftFiltering(im,3,3)
+
+    mask = fgbg.apply(im, 0.5)
+    cv2.imshow('mask', mask)
+
 def handleDisparity(msg):
     global br
     im = br.imgmsg_to_cv2(msg.image)
@@ -62,7 +75,7 @@ def handleDisparity(msg):
         [.07,.12,.07]
         ],np.float32)
 
-    proc = cv2.dilate(im, k_dilate, iterations = 5)
+    proc = cv2.dilate(im, k_dilate, iterations = 5) # fill the holes
 
     #proc = cv2.GaussianBlur(proc,(13,13),0) 
 
@@ -70,7 +83,7 @@ def handleDisparity(msg):
 
     print n # print number of blobs
 
-    cv2.imshow('image', im)
+    cv2.imshow('disp', im)
     cv2.imshow('identified', identified)
 
     cv2.waitKey(100)
@@ -80,11 +93,14 @@ def main():
     #dtype, n_channels = br.encoding_to_cvtype2('8UC3')
     rospy.Subscriber("my_stereo/points2", PointCloud2, handleCloud)
     rospy.Subscriber("my_stereo/disparity", DisparityImage, handleDisparity)
-    #rospy.Subscriber("my_stereo/left/image_rect", Image, handleDisparity)
+    rospy.Subscriber("my_stereo/left/image_rect_color", Image, handleImage)
 
-    win = cv2.namedWindow('image')
+    win1 = cv2.namedWindow('disp')
+    win2 = cv2.namedWindow('image')
+    win3 = cv2.namedWindow('identified')
 
     rospy.init_node("cloudHandler", anonymous=True)
+
     try:
         rospy.spin()
     except KeyboardInterrupt:
