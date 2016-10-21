1764ff8ea3763cc71b98cb554d0f9796ec348f78
lzuehsow <lydiazuehsow@gmail.com>
2016-10-21 17:18:21 -0400

    Added optical flow

--- Stereo/handleStereoData.py
+++ Stereo/handleStereoData.py
@@ -63,6 +63,31 @@ def handleImage(msg):
     mask = fgbg.apply(im, 0.5)
     cv2.imshow('mask', mask)
 
+#New code
+    if(prvs):
+        ret, frame1 = cap.read()
+        prvs = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
+        hsv = np.zeros_like(frame1)
+        hsv[...,1] = 255
+
+        while(1):
+            ret, frame2 = cap.read()     
+            next = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
+
+            flow = cv2.calcOpticalFlowFarneback(prvs,next, 0.5, 3, 15, 3, 5, 1.2, 0)
+
+            mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
+            hsv[...,0] = ang*180/np.pi/2
+            hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+            bgr = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+
+            cv2.imshow('frame2',bgr)
+            k = cv2.waitKey(30) & 0xff
+
+            prvs = next
+
+
+
 def handleDisparity(msg):
     global br
     im = br.imgmsg_to_cv2(msg.image)
@@ -86,7 +111,7 @@ def handleDisparity(msg):
     cv2.imshow('disp', im)
     cv2.imshow('identified', identified)
 
-    cv2.waitKey(100)
+    cv2.waitKey(1000)
     #print "MIN : {}, MAX : {}".format(disparity.min_disparity, disparity.max_disparity)
 
 def main():
