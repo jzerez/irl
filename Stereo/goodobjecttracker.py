6380f2c3e9f04ad67082a855a44c4cf7f4799e1e
lzuehsow <lydiazuehsow@gmail.com>
2016-12-03 17:25:42 -0500

    Working now

--- Stereo/goodobjecttracker.py
+++ Stereo/goodobjecttracker.py
@@ -25,23 +25,50 @@ class Target(object):
 		self.firstframe = None
 		self.thresh = None
 
-		# self.yellowLower = (max(0, 20), 0, 0)
-		# self.yellowUpper = (min(180, 50), 255, 255)
 		self.yellowLower = (20, 100, 100)
-		self.yellowUpper = (262, 274, 255)
+		# self.yellowUpper = (262, 274, 255)
+		self.yellowUpper = (30, 255, 255)
 
-		self.greenLower = (29,30,6)
-		self.greenUpper = (64,255,255)
+		self.greenLower = (45,100,100)
+		self.greenUpper = (75,255,255)
+
+		self.blueLower = (90,100,100)
+		self.blueUpper = (125,255,255)
 
 		self.Lower = [self.yellowLower,self.greenLower]
 		self.Upper =[self.yellowUpper,self.greenUpper]
 
-		self.objectlist = [[0,(0,0)]] # Radius, XY coordinates.
+		self.objectlist = [[0,(0,0),'Null']] # Radius, XY coordinates.
 
 class Find_Color(object):
 	def __init__(self, frame):
 		self.blurred = cv2.GaussianBlur(frame, (21, 21), 0)
-	# def findcolor(self):
+		self.objcolor = "Null"
+
+	def findobject(self):
+		if counter <= 15:
+			print "Calib"
+		if counter >= 10:
+			# t.objectlist.append([radius,(center[0],center[1])])
+
+			temp = False
+			objnum = 0
+
+			for obj in t.objectlist:
+				objrad = obj[0]
+
+				if ((radius - 25.0) <= objrad) and ((radius + 25.0) >= objrad):
+					print 'Found object:' , objnum
+					# print 'List is now: ' , t.objectlist
+					temp = True
+
+				objnum += 1
+				
+			if temp == False:
+				t.objectlist.append([radius,(0,0),fc.objcolor])
+				print 'Added object to list:' , [radius,(int(center[0]), int(center[1])),str(fc.objcolor)]
+				print fc.objcolor
+				print 'List is now: ' , t.objectlist
 
 
 
@@ -52,12 +79,16 @@ if __name__=="__main__":
 
 	while True:
 		(grabbed, frame) = t.camera.read()
+
 		# if the frame could not be grabbed, then we have reached the end
 		# of the video
 
 		# resize the frame, convert it to grayscale, and blur it
-		t.frame = imutils.resize(frame, width=500)
+		#t.frame = imutils.resize(frame, width=500)
+		t.frame = frame
 		hsv = cv2.cvtColor(t.frame, cv2.COLOR_BGR2HSV)
+		fc = Find_Color(hsv)
+		
 		gray = cv2.cvtColor(t.frame, cv2.COLOR_BGR2GRAY)
 		gray = cv2.GaussianBlur(gray, (21, 21), 0)
 		# if the first frame is None, initialize it
@@ -67,7 +98,6 @@ if __name__=="__main__":
 		else:
 			if t.count == 0:
 				t.firstframe = gray
-				fc = Find_Color(hsv)
 				t.count += 1
 
 			if t.thresh is not None:
@@ -84,43 +114,31 @@ if __name__=="__main__":
 					(center,radius) = cv2.minEnclosingCircle(c)
 					if radius >= 50:
 						# print "radius: " , radius
-						if center[0] >= 375:
-							center = (374,center[1])
-
+						
 						cv2.circle(t.frame, (int(center[0]), int(center[1])), 5, (0,255,0), -1)
 						cv2.circle(t.frame, (int(center[0]), int(center[1])), int(radius), (0,255,0), 5)
 						# print "center", center
-						color = fc.blurred[int(center[0]),int((center[1]))]
+						
+						cv2.imshow('blr', fc.blurred)
 
+						color = fc.blurred[int(center[1]),int((center[0]))]
+						# print color[0]
 						# print radius
 
 						if ((color[0] <= t.yellowUpper[0]) and (color[0] >= t.yellowLower[0])):
 							# print 'Found yellow object'
 							# print center
 							# print color
-
-							if counter <= 15:
-								print "Calib"
-							if counter >= 10:
-								# t.objectlist.append([radius,(center[0],center[1])])
-
-								temp = False
-								objnum = 0
-
-								for obj in t.objectlist:
-									objrad = obj[0]
-
-									if ((radius - 25.0) <= objrad) and ((radius + 25.0) >= objrad):
-										print 'Found object:' , objnum
-										# print 'List is now: ' , t.objectlist
-										temp = True
-
-									objnum += 1
-									
-								if temp == False:
-									t.objectlist.append([radius,(0,0)])
-									print 'Added object to list:' , [radius,(int(center[0]), int(center[1]))]
-									print 'List is now: ' , t.objectlist
+							fc.objcolor = "Yellow"
+							fc.findobject()
+							counter +=1 
+						if ((color[0] <= t.greenUpper[0]) and (color[0] >= t.greenLower[0])):
+							fc.objcolor = "Green"
+							fc.findobject()
+							counter +=1
+						if ((color[0] <= t.blueUpper[0]) and (color[0] >= t.blueLower[0])):
+							fc.objcolor = "Blue"
+							fc.findobject()
 							counter +=1 
 
 			cv2.imshow("Camera", t.frame)
@@ -144,102 +162,3 @@ if __name__=="__main__":
 			break
 		if key == ord("r"):
 			t.firstframe = gray
-
-
-# class Cluster(object):
-
-#     def __init__(self):
-#         self.pixels = []
-#         self.centroid = None
-
-#     def addPoint(self, pixel):
-#         self.pixels.append(pixel)
-
-#     def setNewCentroid(self):
-
-#         R = [colour[0] for colour in self.pixels]
-#         G = [colour[1] for colour in self.pixels]
-#         B = [colour[2] for colour in self.pixels]
-
-#         R = sum(R) / len(R)
-#         G = sum(G) / len(G)
-#         B = sum(B) / len(B)
-
-#         self.centroid = (R, G, B)
-#         self.pixels = []
-
-#         return self.centroid
-
-
-# class Kmeans(object):
-
-#     def __init__(self, k=3, max_iterations=5, min_distance=5.0, size=200):
-#         self.k = k
-#         self.max_iterations = max_iterations
-#         self.min_distance = min_distance
-#         self.size = (size, size)
-
-#     def run(self, image):
-#         self.image = image
-#         self.image.thumbnail(self.size)
-#         self.pixels = numpy.array(image.getdata(), dtype=numpy.uint8)
-
-#         self.clusters = [None for i in range(self.k)]
-#         self.oldClusters = None
-
-#         randomPixels = random.sample(self.pixels, self.k)
-
-#         for idx in range(self.k):
-#             self.clusters[idx] = Cluster()
-#             self.clusters[idx].centroid = randomPixels[idx]
-
-#         iterations = 0
-
-#         while self.shouldExit(iterations) is False:
-
-#             self.oldClusters = [cluster.centroid for cluster in self.clusters]
-
-#             print iterations
-
-#             for pixel in self.pixels:
-#                 self.assignClusters(pixel)
-
-#             for cluster in self.clusters:
-#                 cluster.setNewCentroid()
-
-#             iterations += 1
-
-#         return [cluster.centroid for cluster in self.clusters]
-
-#     def assignClusters(self, pixel):
-#         shortest = float('Inf')
-#         for cluster in self.clusters:
-#             distance = self.calcDistance(cluster.centroid, pixel)
-#             if distance < shortest:
-#                 shortest = distance
-#                 nearest = cluster
-
-#         nearest.addPoint(pixel)
-
-#     def calcDistance(self, a, b):
-
-#         result = numpy.sqrt(sum((a - b) ** 2))
-#         return result
-
-#     def shouldExit(self, iterations):
-
-#         if self.oldClusters is None:
-#             return False
-
-#         for idx in range(self.k):
-#             dist = self.calcDistance(
-#                 numpy.array(self.clusters[idx].centroid),
-#                 numpy.array(self.oldClusters[idx])
-#             )
-#             if dist < self.min_distance:
-#                 return True
-
-#         if iterations <= self.max_iterations:
-#             return False
-
-#         return True
